1a6ea26fdfe31f948ccd997876414beede397a86
Ivan Sentemon <sentemon.i@gmail.com>
2024-01-11 09:12:57 +0100

    Revert "Initial commit"
    This reverts commit 8dd723c204f6ee792824d612736ab92654519722.

--- CodeWars/BetterThanAverage.cs
+++
@@ -1,8 +0,0 @@
-using System.Linq;
-public class Kata
-{
-  public static bool BetterThanAverage(int[] ClassPoints, int YourPoints)
-  {
-    return YourPoints > ClassPoints.Average();
-  }
-}
--- CodeWars/IsDivisible.cs
+++
@@ -1,7 +0,0 @@
-public class DivisibleNb 
-{
-	public static bool IsDivisible(int n, int x, int y) 
-  {
-    return n % x == 0 && n % y == 0;
-	}
-}
