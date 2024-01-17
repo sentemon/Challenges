0fa48f5314117754b96059fbce5bb69830434bdb
Ivan Sentemon <sentemon.i@gmail.com>
2024-01-17 21:19:04 +0100

    Not very secure

---
+++ CodeWars/Alphanumeric.cs
@@ -0,0 +1,20 @@
+public class Kata
+{
+  public static bool Alphanumeric(string str)
+  {
+    string alphanumeric = "abcdefghijklmnopqrstuvwxyz0123456789";
+    if (str != null && str != "")
+    {
+      foreach (char chr in str.ToLower())
+      {
+        if (!(alphanumeric.Contains(chr)))
+        {
+          return false;
+        }
+      }
+
+      return true;
+    }
+    return false;
+  }
+}
