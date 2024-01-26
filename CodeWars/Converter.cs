38429489558103968186ee10b70ee5ad40c982f3
Ivan Sentemon <sentemon.i@gmail.com>
2024-01-26 20:57:49 +0100

    Miles per gallon to kilometers per liter

--- CodeWars/Converter.cs
+++ CodeWars/Converter.cs
@@ -4,7 +4,7 @@ public static class Kata
 {
 	public static double Converter(int mpg)
 	{
-		double imperialGallon = 4.54609188;
+	double imperialGallon = 4.54609188;
     double mile = 1.609344;
     
     double result = (mpg * mile) / imperialGallon;
