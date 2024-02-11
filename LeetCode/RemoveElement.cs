688e1117cff1e3bc6b4fcb15b6423f8b647594c0
Ivan Sentemon <sentemon.i@gmail.com>
2024-02-11 09:14:22 +0100

    27. Remove Element

---
+++ LeetCode/RemoveElement.cs
@@ -0,0 +1,19 @@
+public class Solution 
+{
+    public int RemoveElement(int[] nums, int val)
+    {
+        int k = 0;
+
+        for (int i = 0; i < nums.Length; i++) 
+        {
+            if (nums[i] != val)
+            {
+                nums[k] = nums[i];
+                k++;
+            }
+            
+        }
+
+        return k;
+    }
+}
