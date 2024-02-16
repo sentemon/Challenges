5a2c13db415ae5afbecb1887732faf18d9e5dcec
Ivan Sentemon <sentemon.i@gmail.com>
2024-02-16 10:14:09 +0100

    1791. Find Center of Star Graph

---
+++ LeetCode/FindCenter.cs
@@ -0,0 +1,10 @@
+public class Solution 
+{
+    public int FindCenter(int[][] edges)
+    {
+        if (edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1])
+            return edges[0][0];
+        else 
+            return edges[0][1];
+    }
+}
