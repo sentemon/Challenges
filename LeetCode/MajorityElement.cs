1d77b747f6a4abd12290eb2f5782c62ac71ee18d
Ivan Sentemon <sentemon.i@gmail.com>
2024-02-21 14:02:58 +0100

    169. Majority Element

---
+++ LeetCode/MajorityElement.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int MajorityElement(int[] nums) {
+        int count = 0;
+        int candidate = 0;
+
+        foreach (int num in nums) 
+        {
+            if (count == 0) 
+                candidate = num;
+            count += (num == candidate) ? 1 : -1;
+        }
+
+        return candidate;
+    }
+}
