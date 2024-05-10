9cd7031010de06748acfafd74dc1dcf9ab93d57b
Ivan Sentemon <sentemon.i@gmail.com>
2024-05-11 00:23:32 +0200

    2520. Count the Digits That Divide a Number

---
+++ Python/LeetCode/countDigits.py
@@ -0,0 +1,10 @@
+# 2520. Count the Digits That Divide a Number
+class Solution:
+    def countDigits(self, num: int) -> int:
+        count = 0
+
+        for i in str(num):
+            if num % int(i) == 0:
+                count += 1
+        
+        return count        
