5e6e21646dafb4494d503ebf3584efeda890466b
Ivan Sentemon <sentemon.i@gmail.com>
2024-05-20 22:04:42 +0200

    Chuck Norris VII - True or False?

---
+++ CSharp/CodeWars/8kyu/IfChuckSaysSo.cs
@@ -0,0 +1,10 @@
+// Chuck Norris VII - True or False? (Beginner) (sry for this, I just need a commit)
+using System;
+
+public static class Kata
+{
+  public static bool IfChuckSaysSo()
+  {
+    return 0 == 1;
+  }
+}
