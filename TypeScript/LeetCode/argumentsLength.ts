07b0f9f1efdf0500be51e566008e2f88199267af
Ivan Sentemon <sentemon.i@gmail.com>
2024-10-06 09:31:51 +0200

    2703. Return Length of Arguments Passed

---
+++ TypeScript/LeetCode/argumentsLength.ts
@@ -0,0 +1,11 @@
+// 2703. Return Length of Arguments Passed
+
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+
+function argumentsLength(...args: JSONValue[]): number {
+    return args.length;
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
